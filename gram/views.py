34e47e598f1d4e4e735d8f3c2d57868b6f98fcfa
Maratah <maratah7@gmail.com>
2020-08-09 22:52:08 +0300

    add imports. - add class as input.

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+from .models import Post
+from django.views.generic import (
+    ListView
+)
+
 
 # Create your views here.
+class PostListView(ListView):
+    template_name = "post.html"
+    queryset = Post.objects.all()
+    context_object_name = "posts"
