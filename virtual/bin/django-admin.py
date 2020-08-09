f6b5fd1e548ed56afb2bce5704446a371dea7450
Maratah <maratah7@gmail.com>
2020-08-09 22:16:13 +0300

    delete project. delete app.

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/moringa/Documents/gram/virtual/bin/python3.6
+#!/home/moringa/Documents/gram/virtual/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
