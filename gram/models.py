18152c4a5227a2caac0afd43794cefdf34932cb9
Maratah <maratah7@gmail.com>
2020-08-12 22:54:43 +0300

    modify input. - solve a value error.

--- gram/models.py
+++ gram/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils import timezone
-
+from datetime import datetime
+from django.conf import settings 
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    image = models.ImageField(blank=True,null=True)
+    author  = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='posts')
+    image   = models.ImageField(default='default.png', blank=True)
     caption = models.TextField()
+    likes   = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='post_likes')
     date_posted = models.DateTimeField(default=timezone.now)
+    saved   = models.BooleanField(default=False)  
 
-
+    def get_absolute_url(self):
+        return reverse('gram:post_detail', kwargs={"id":self.id})
+    
     def __str__(self):
-        return self.caption  
+        return self.caption 
